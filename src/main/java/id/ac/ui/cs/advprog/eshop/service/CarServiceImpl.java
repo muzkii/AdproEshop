82476a09c9d3589048c7713a825b6387c63f8445
muzkii <andriyo.averill@ui.ac.id>
2025-02-25 20:20:27 +0700

    feat: changed CarServiceImpl to depend on IRepository directly from applying LSP

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -4,28 +4,23 @@ import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.repository.ICarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 
 @Service
-public class CarServiceImpl implements CarService{
-    
+public class CarServiceImpl implements CarService {
+
     @Autowired
     private ICarRepository carRepository;
-    
+
     @Override
     public Car create(Car car) {
-        carRepository.create(car);
-        return car;
+        return carRepository.create(car);
     }
 
     @Override
     public List<Car> findAll() {
-        List<Car> allCar = new ArrayList<>();
-        Iterator<Car> carIterator = carRepository.findAll();
-        carIterator.forEachRemaining(allCar::add);
-        return allCar;
+        return carRepository.findAll();
     }
 
     @Override
@@ -35,7 +30,7 @@ public class CarServiceImpl implements CarService{
 
     @Override
     public void update(String carId, Car car) {
-        carRepository.update(carId, car);
+        carRepository.update(car);
     }
 
     @Override
