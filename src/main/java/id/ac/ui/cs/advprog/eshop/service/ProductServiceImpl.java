97a6dfbb37c9605116b7654eac7b522b2c4f6bdc
muzkii <andriyo.averill@ui.ac.id>
2025-02-25 20:20:46 +0700

    feat: changed ProductServiceImpl to depend on IRepository directly from applying LSP

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -5,8 +5,7 @@ import id.ac.ui.cs.advprog.eshop.repository.IProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 
 @Service
@@ -23,9 +22,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> findAll() {
-        Iterator<Product> productIterator = productRepository.findAll();
-        List<Product> allProduct = new ArrayList<>();
-        productIterator.forEachRemaining(allProduct::add);
+        List<Product> allProduct = productRepository.findAll();
         return allProduct;
     }
     
