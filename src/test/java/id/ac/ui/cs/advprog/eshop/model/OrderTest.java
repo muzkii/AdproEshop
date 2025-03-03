cc2590e5c60d6107de978e6d70a4333b676c98c4
muzkii <andriyo.averill@ui.ac.id>
2025-03-03 20:45:56 +0700

    [RED] Add tests for Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class OrderTest {
     private List<Product> products;
